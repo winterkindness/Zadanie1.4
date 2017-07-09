2d88e8ad6980db4d0aad142939b0270be6e39f89
Dmitry Alexandrov <rus22imp@mail.ru>
2017-07-09 22:02:38 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,n,p=1;
+            string str = Console.ReadLine();
+            string[] s = str.Split('*');
+            n = s.Length;
+            for (i = 0; i < n; i++)
+                p *= Convert.ToInt32(s[i]);
+            Console.WriteLine(str + " = " + p);
+            Console.ReadKey();  
+        }
+    }
+}
